067f2ad1d664c0d86189f6d415b04a21596b5ffe
Hack.cpp <72662612+HPD1155@users.noreply.github.com>
2021-02-22 10:03:38 -0600

    Update loading.cs

--- loading.cs
+++ loading.cs
@@ -1,11 +1,4 @@
-﻿/*
- * Created by SharpDevelop.
- * User: aadi_
- * Date: 2/20/2021
- * Time: 1:18 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+
 using System;
 using System.ComponentModel;
 using System.Drawing;
